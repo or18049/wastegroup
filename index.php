06d65c21340d5340ad6c2776764917d75c269f95
or18049 <90588787+or18049@users.noreply.github.com>
2021-10-21 15:08:40 +0900

    topページをindexに

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
-<?php>
-  header("location:top.php");
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>ログイン</title>
+ </head>
+ <body>
+ <?php
+   session_start();
+   $_SESSION["manage"] = false;
+   ?>
+    これはトップページだよ
+    <a href=login.php>ログインページ<br>
+</body>
+</html>
