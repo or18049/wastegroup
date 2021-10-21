f0b2328c2080d9b373bf89d067628a94af3ee0fb
or18049 <90588787+or18049@users.noreply.github.com>
2021-10-21 15:26:39 +0900

    トップページっぽく

--- index.php
+++ index.php
@@ -2,14 +2,15 @@
 <html>
  <head>
   <meta charset="utf-8">
-  <title>ログイン</title>
+  <title>ゴミ分類検索ページ</title>
  </head>
  <body>
  <?php
    session_start();
    $_SESSION["manage"] = false;
    ?>
-    これはトップページだよ
-    <a href=login.php>ログインページ<br>
+    <h1>ゴミ分類検索ページ</h1><br>
+    ゴミの分別を検索したい方は<a href=search.php>こちら<a><br>
+    管理者の方は<a href=login.php>こちら
 </body>
 </html>
