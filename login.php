ac9fa7ddf1ead988226c57195c5e7ec94381a095
or18049 <90588787+or18049@users.noreply.github.com>
2021-10-21 14:47:49 +0900

    ログインボタンが押されたときに判定

--- login.php
+++ login.php
@@ -10,29 +10,31 @@
    session_start();
    $_SESSION["id"] = $_POST["id"];
    $_SESSION["pass"] = $_POST["pass"];
-   if((empty($_SESSION["id"])) || (empty($_SESSION["pass"]))){
-    header("location: top.html");
-    $_SESSION["miss"] = true;
-  }
+   if(isset($_POST['check'])){
+    if((empty($_SESSION["id"])) || (empty($_SESSION["pass"]))){
+        header("location: top.php");
+        $_SESSION["miss"] = true;
+    }
 
-  if(preg_match("/<(\"[^\"]*\"|'[^']*'|[^'\">])*>/", $_POST["id"])){
-    header("location: top.html");
-    $_SESSION["miss"] = true;
-  }
-  if(preg_match("/<(\"[^\"]*\"|'[^']*'|[^'\">])*>/", $_POST["pass"])){
-    header("location: top.html");
-    $_SESSION["miss"] = true;
-  }
-  if(($_SESSION["id"] == "or18049") && ($_SESSION["pass"] == "abc")){
-      header("manage_top.php");
-      $_SESSION["manage"] = true;
-  }
+    if(preg_match("/<(\"[^\"]*\"|'[^']*'|[^'\">])*>/", $_POST["id"])){
+        header("location: top.php");
+        $_SESSION["miss"] = true;
+    }
+    if(preg_match("/<(\"[^\"]*\"|'[^']*'|[^'\">])*>/", $_POST["pass"])){
+        header("location: top.php");
+        $_SESSION["miss"] = true;
+    }
+    if(($_SESSION["id"] == "or18049") && ($_SESSION["pass"] == "abc")){
+        header("manage_top.php");
+        $_SESSION["manage"] = true;
+    }
+   }
    ?>
   <form action=login.php method=POST>
    <input type=text name=id size=5>
    <input type=text name=pass size=5>
    <br>
-   <button type=submit>ログイン</botton>
+   <button type=submit name=check>ログイン</botton>
  </form>
  <hr>
  <a href=top.php>トップページへ<br>
