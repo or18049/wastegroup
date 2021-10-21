72152693298bac8b6656d247bc3e2dfafd0554fd
or18049 <90588787+or18049@users.noreply.github.com>
2021-10-21 15:35:11 +0900

    トップページっぽく

--- manager_index.php
+++ manager_index.php
@@ -5,7 +5,8 @@
   <title>ログイン</title>
  </head>
  <body>
-    これは管理トップページだよ
-    <a href=login.php>ログインページ<br>
+    <h1>管理者用ゴミ分類検索ページ</h1><br>
+    ゴミの分別の検索は<a href=search.php>こちら</a><br>
+    新規分別情報の追加は<a href=add_form.html>こちら
 </body>
 </html>
